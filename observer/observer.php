c920a08c0792182328e17ea09f1d039ce2740e05
hongcheng.xu <819664855@qq.com>
2016-01-22 16:17:29 +0800

    优化观察者模式

--- observer/observer.php
+++ observer/observer.php
@@ -1,7 +1,9 @@
+
 <?php
 
     interface IObserver{
-        function onChanged( $sender, $args );
+        function onSendMsg( $sender, $args );
+        function getName();
     }
 
     interface IObservable{
@@ -11,30 +13,52 @@
     class UserList implements IObservable{
         private $_observers = array();
 
-        public function addCustomer( $name ){
+        public function sendMsg( $name ){
             foreach( $this->_observers as $obs ){
-                $obs->onChanged( $this, $name );
+                $obs->onSendMsg( $this, $name );
             }
         }
 
         public function addObserver( $observer ){
             $this->_observers[]= $observer;
         }
+
+        public function removeObserver($observer_name) {
+            foreach($this->_observers as $index => $observer) {
+                if ($observer->getName() === $observer_name) {
+                    array_splice($this->_observers, $index, 1);
+                    return;
+                }
+            }
+        }
     }
 
     class UserListLogger implements IObserver{
-        public function onChanged( $sender, $args ){
-            echo( "'$args' added to user list\n" );
+        public function onSendMsg( $sender, $args ){
+            echo( "'$args' send to UserListLogger\n" );
+        }
+
+        public function getName(){
+            return 'UserListLogger';
+        }
+    }
+
+    class OtherObserver implements IObserver{
+        public function onSendMsg( $sender, $args ){
+            echo( "'$args' send to OtherObserver\n" );
+        }
+
+        public function getName(){
+            return 'OtherObserver';
         }
     }
 
 
     $ul = new UserList();//被观察者
+    $ul->addObserver( new UserListLogger() );//增加观察者
+    $ul->addObserver( new OtherObserver() );//增加观察者
+    $ul->sendMsg( "Jack" );//发送消息到观察者
 
-    $obj1 = new UserListLogger();
-    $obj2 = new UserListLogger();
-    $ul->addObserver( $obj1 );//增加观察者
-    $ul->addObserver( $obj2 );//增加观察者
-    
-    $ul->addCustomer( "Jack" );//发送消息到观察者
+    $ul->removeObserver('UserListLogger');//移除观察者
+    $ul->sendMsg("hello");//发送消息到观察者
 ?>
